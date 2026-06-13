9938e027b5015460ec7de7004dfa2952098453ba
weijiej2964 <73482897+weijiej2964@users.noreply.github.com>
2026-06-13 17:20:57 -0400

    Time: 135 ms (11.03%) | Memory: 28.5 MB (60.15%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -11,7 +11,7 @@ class Solution:
         stack.append(0)
 
         for i in range(1,len(temperatures)):
-            while len(stack) > 0 and temperatures[i] > temperatures[stack[-1]]:
+            while stack and temperatures[i] > temperatures[stack[-1]]:
                 an[stack[-1]] = i - stack[-1]
                 stack.pop()
             stack.append(i)
