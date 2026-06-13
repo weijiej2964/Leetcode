ac7e631f4e2d3679eb4cce058ff6ec8e0ca1d8f7
weijiej2964 <73482897+weijiej2964@users.noreply.github.com>
2026-06-13 17:19:49 -0400

    Time: 143 ms (8.17%) | Memory: 28.1 MB (88.54%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #brute force, for each index, loop forward to find warmer day
+        #Use a stack, storing index
+            #Loop through temperatures, check current index compared to top of stack
+            #if current index is greater than top of stack keep poping it until its not
+            # Add the current index into the stack
+            #After loop ends, append len(stack) 0s solved by init arr size
+        an = [0] * len(temperatures) 
+        stack = deque()
+        stack.append(0)
+
+        for i in range(1,len(temperatures)):
+            while len(stack) > 0 and temperatures[i] > temperatures[stack[-1]]:
+                an[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return an
+
+
